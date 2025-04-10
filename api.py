e20cb631a22de2e91ce48d379dcae59b0e9c428b
Yan Gurenko <73452308+gurenko-yan@users.noreply.github.com>
2025-04-10 23:26:15 +0700

    Large update
    Added page caching
    Added output
    Added data handling

--- api.py
+++ api.py
@@ -1,50 +1,69 @@
 from bs4 import BeautifulSoup
 import requests
 
-page = requests.get('http://93.92.65.26/aspx/GorodM.htm')
-page.encoding = 'windows-1251'
-soup = BeautifulSoup(page.text, 'html.parser')
-
-
 class api:
     def __init__(self):
+        self.page = requests.get('http://93.92.65.26/aspx/GorodM.htm')
+        self.page.encoding = 'windows-1251'
+        self.soup = BeautifulSoup(self.page.text, 'html.parser')
         self.root = self.get_tag(['div', 'table'])
-
+    
     def find_data(self, district, street):
         tr = self.root.find('tr') # поиск первого тега <tr>
 
-        while tr['height'] != '0': # поиск <tr> height != 0
-            if tr['height'] == '20': # <tr> height = 20
-                td = tr.find_all('td')[1]
-                text = self.correct(td.get_text())
+        try:
+            tr['height']
+        except:
+            cache = open('cache.txt', 'r', encoding='utf-8')
+            text = cache.read()
+            cache.close()
+            
+            if not(text):
+                return -1
 
-                if text == self.correct(district):
+            self.soup = BeautifulSoup(text, 'html.parser')
+            self.root = self.get_tag(['div', 'table'])
+            
+            tr = self.root.find('tr')
+        else:
+            cache = open('cache.txt', 'w', encoding='utf-8')
+            cache.write(self.page.text)
+            cache.close()
+
+        
+        while tr['height'] != '0': # поиск строки с названием района
+            if tr['height'] == '20':
+                td = tr.find_all('td')[1]
+                text = self.only_alpha(td.get_text())
+
+                if text == self.only_alpha(district):
                     tr = tr.find_next('tr') # след. <tr> с которого начинается поиск улицы
                     break
 
             tr = tr.find_next('tr')
 
         while not(tr['height'] in ['20', '0']): # поиск <tr> height != 20 != 0
-            td = tr.find_all('td')[1] # <td> с улицами
-            is_correct = self.check_street(td.get_text(), street)
+            td = tr.find_all('td')
+            is_correct = self.check_street(td[1].get_text(), street)
             if is_correct:
-                data = tr.find_all('td')
-                data_text = []
-                for i in data:
-                    data_text.append(i.get_text())
-                return data_text
+                data = []
+
+                for i in td:
+                    data.append(i.get_text())
+
+                return self.structuring(data)
 
             tr = tr.find_next('tr')
     
     def get_tag(self, hierarchy): # получение тега в иерархии
-        current_tag = soup.body
+        current_tag = self.soup.body
 
         for tag in hierarchy:
             current_tag = current_tag.find(tag)
 
         return current_tag
     
-    def correct(self, text): # преобразование текста в строку без не буквенных символов
+    def only_alpha(self, text): # преобразование текста в строку без не буквенных символов
         result_text = ''
 
         for let in text:
@@ -60,19 +79,38 @@ class api:
                 return False
             elif street in string:
                 return True
+    
+    def correct_data(self, data, flag):
+        structured_data = []
+        s = ''
+        is_excessive_space = False
+
+        for let in data:
+            if not let in ['\n', '\r']:
+                if let == ' ':
+                    if not is_excessive_space:
+                        s += let
+                        is_excessive_space = True
+                else:
+                    s += let
+                    is_excessive_space = False
+            elif len(s) and flag:
+                structured_data.append(s)
+                s = ''
         
+        structured_data.append(s)
+
+        return structured_data
+
     def structuring(self, data):
         structured_data = []
-        for d in data:
-            string = ''
-            for let in d:
-                if let == '\\':
-                    if len(string):
-                        structured_data += [string]
-                        string = ''
-                        
-                string += let
+        structured_data += self.correct_data(data[0], 1)
+        structured_data += self.correct_data(data[1], 0)
+        structured_data += self.correct_data(data[2], 1)
+
+        return structured_data
 
 
 test = api()
-print(test.find_data('Октябрьский район', '2-я Мелькомбинатская'))
+data = test.find_data('Октябрьский район', 'Цимлянская')
+print(data)
